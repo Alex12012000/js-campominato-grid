77d5450c4232ce8e2cbd62722d3a81cb8cc91868
Alex12012000 <arcex.29@gmail.com>
2022-06-14 20:29:04 +0200

    Finito

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ let userDifficulty;
 let maxNumbers = 0;
 
 // Definisco la difficoltà in base alla scelta fatta dall'utente 
+// Chiedo all'utente la difficoltà finche non inserisce un numero corretto
 while(userDifficulty < 1 || userDifficulty > 3 || isNaN(userDifficulty)){
 
     userDifficulty = parseInt(prompt('Scegli il livello di difficoltà tra 1 - 2 - 3'));
@@ -28,49 +29,50 @@ while(userDifficulty < 1 || userDifficulty > 3 || isNaN(userDifficulty)){
     } else if (userDifficulty === 3) {
         maxNumbers = 49;
     } else {
-        alert('Inserisci un numero corretto')
+        alert('Inserisci un numero corretto');
     }
 }
 
+// Tentativi dell'utente
+let maxAttempts = maxNumbers - 16;
 // Variabile che racchiude l'array con le bombe generate
 let bomb = bombGenerator(16, 1, maxNumbers);
-console.log(bomb)
 
 
-// Tentativi dell'utente
-let maxAttempts = 18 - 16;
-
+// Numeri inseriti dall'utente
 let userInputArray = [];
 // finche l'utente inserisce un numero che non è presente nell'array di bombe
 // continua a chiedere un numero finche non arriva al numero massimo di tentativi
 // se l'utente inserisce un numero che è presente all'interno dell'array bombe, l'utente perde
-let game = true
+let game = true;
 while(game) {
 
-    let userInput = parseInt(prompt('Dimmi un numero'))
+    let userInput = parseInt(prompt('Dimmi un numero'));
     
     // se l'utente inserisce un numero diverso da quello inserito precedentemente il gioco continua
     // altrimenti continua a chidere un numero diverso
     if (userInput < 1 || userInput > maxNumbers) {
-        alert ('Numero non corretto')
-    } else if (!userInputArray.includes(userInput)) {
-        userInputArray.push(userInput)
+        alert ('Numero non corretto');
+    } else if (!userInputArray.includes(userInput) && !bomb.includes(userInput)) {
+        userInputArray.push(userInput);
     }
     // se l'utente inserisce un numero che si trova anche all'interno dell'array di bombe, il gioco finisce
     // Se l'utente inserisce un numero diverso dal numero delle bombe per il numero di tentativi totali, l'utente vince
     // Alert di vittoria + punteggio
     if(bomb.includes(userInput)) {
         game = false;
-        alert('hai perso')
+        alert('hai perso');
+        alert ('l tuo punteggio è ' + userInputArray.length);
     } else if(userInputArray.length === maxAttempts) {
         game = false;
-        alert('Hai vinto')
-        alert ('l tuo punteggio è ' + userInputArray.length)
+        alert('Hai vinto');
+        alert ('l tuo punteggio è ' + userInputArray.length);
     }
 
-    console.log(userInputArray);
 }
-
+// PRINT
+console.log(userInputArray);
+console.log(bomb);
 
 
 // ------------------------
